0dd95d4fa05a2383a43dd6648a6996e4ae342149
UserIT <UserIT@ITADMIN>
2022-12-30 16:09:36 +0800

    Add project files.

---
+++ MapsPage/MapsPage/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.Xaml;
+
+namespace MapsPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonOpenCoords_Clicked(object sender, EventArgs e)
+        {
+            if (!double.TryParse(EntryLatitude.Text, out double lat))
+                return;
+
+            if (!double.TryParse(EntryLongitude.Text, out double lng))
+                return;
+
+            await Map.OpenAsync(lat, lng, new MapLaunchOptions
+            {
+                Name = EntryName.Text,
+                NavigationMode = NavigationMode.None
+            });
+
+        }
+    }
+}
